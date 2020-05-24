564abeaf003e1e20c1f35d87635c03aff78e2869
tyobigoro_i <tyobigorox@gmail.com>
2020-05-24 17:40:57 +0900

    c

--- tView/ViewController.swift
+++ tView/ViewController.swift
@@ -10,11 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        setLabelCenter?()
+        
+    }
+    
+    fileprivate lazy var setLabelCenter: (() -> Void)? = {
+        self.label.center.x = self.view.center.x
+        self.label.center.y = self.view.center.y * 0.25
+        return nil
+    }()
+    
+    
 }
 
