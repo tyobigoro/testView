ea380574f3f021ec21faa8c6800d137ef863a365
tyobigoro_i <tyobigorox@gmail.com>
2020-05-26 19:11:05 +0900

    c

--- tView/AppDelegate.swift
+++ tView/AppDelegate.swift
@@ -11,10 +11,12 @@ import UIKit
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        
+        let ud = UserDefaults.standard
+        ud.register(defaults: [Key.firstLunch: true])
+
         return true
     }
 
--- tView/ViewController.swift
+++ tView/ViewController.swift
@@ -1,36 +1,30 @@
-//
-//  ViewController.swift
-//  tView
-//
-//  Created by tyobigoro on 2020/05/24.
-//  Copyright © 2020 tyobigoro. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
-    @IBOutlet weak var label: UILabel!
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
-
-    override func viewDidLayoutSubviews() {
-        super.viewDidLayoutSubviews()
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
         
-        setLabelCenter?()
+        let ud = UserDefaults.standard
         
+        if ud.bool(forKey: Key.firstLunch) {
+            ud.set(false, forKey: Key.firstLunch)
+            
+            guard let vc = self.storyboard?.instantiateViewController(identifier: "BlueVC") as? BlueVC  else { return }
+            self.present(vc, animated: true, completion: nil)
+        }
     }
-    
-    fileprivate lazy var setLabelCenter: (() -> Void)? = {
-        self.label.center.x = self.view.center.x
-        self.label.center.y = self.view.center.y * 0.25
-        return nil
-    }()
-    
     
 }
 
+class BlueVC: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
+
+struct Key {
+    static let firstLunch = "firstLunch"
+}
